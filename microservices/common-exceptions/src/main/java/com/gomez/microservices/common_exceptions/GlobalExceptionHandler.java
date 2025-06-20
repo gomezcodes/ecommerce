389db6c15675a5bdffc8491fd74cf79a9a6dc4b9
Joaquin Gomez <joaquin.gomez.s@outlook.com>
2025-06-20 08:14:39 -0600

    feat: Add exception handling

--- microservices/common-exceptions/src/main/java/com/gomez/microservices/common_exceptions/GlobalExceptionHandler.java
+++ microservices/common-exceptions/src/main/java/com/gomez/microservices/common_exceptions/GlobalExceptionHandler.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -19,7 +22,7 @@ public class GlobalExceptionHandler {
         exception.getBindingResult().getFieldErrors().forEach(error -> {
             errors.put(error.getField(), error.getDefaultMessage());
         });
-
+        log.warn("Validation error: {}", exception);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(errors));
     }
 
@@ -28,7 +31,7 @@ public class GlobalExceptionHandler {
         var errors = new HashMap<String, String>() {{
             put("message", "An error has occurred: Please contact support");
         }};
-
+        log.error("Error: {}", exception.toString());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(errors));
     }
 
